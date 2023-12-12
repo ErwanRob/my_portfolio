75cd60f3f8dd7574ff861c979b95400a4e823a48
Erwan ROBIN <erobin.pro@outlook.fr>
2023-12-12 18:15:50 +0100

    changed about Me into english Added legal mentions modal

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -3,24 +3,37 @@ function AboutMe() {
     <div className="section__about-container">
       <p className="section__about-txt">
         {" "}
-        " Ancien élève ayant entrepris des études en DUT informatique, j'ai
-        décidé, quelques années plus tard, de me plonger à nouveau dans le
-        développement.
+        "Former student who pursued studies in computer science, I decided, a
+        few years later, to immerse myself once again in development.
         <br /> <br />
-        J'ai opté pour la formation d'intégrateur web sur OpenClassrooms, une
-        formation intensive de 9 mois pour devenir développeur Front-end. Cette
-        formation m'a permis d'acquérir des compétences en HTML, CSS et
-        JavaScript. J'ai également eu l'opportunité de découvrir et de
-        travailler avec SASS, React, React-Redux et ses outils. Sans oublier la
-        sensibilisation au référencement des sites web et l'amélioration de leur
-        visibilité via un projet dédié au SEO.
+        I chose to enroll in the web integration program on OpenClassrooms, a
+        9-month intensive course to become a Front-end developer. This program
+        allowed me to acquire skills in HTML, CSS, and JavaScript. I also had
+        the opportunity to discover and work with SASS, React, React-Redux, and
+        their tools. Not to mention the awareness of website SEO and improving
+        their visibility through a dedicated SEO project.
         <br /> <br />
-        Curieux, motivé et avide d'apprendre e nouvelles choses, je poursuis mon
-        apprentissage quotidien à la recherche de nouveaux projets, me
-        permettant d'enrichir mes connaissances. "
+        Curious, motivated, and eager to learn new things, I continue my daily
+        learning, seeking new projects to further enrich my knowledge."
       </p>
     </div>
   );
 }
 
 export default AboutMe;
+
+/* " Ancien élève ayant entrepris des études en DUT informatique, j'ai
+décidé, quelques années plus tard, de me plonger à nouveau dans le
+développement.
+<br /> <br />
+J'ai opté pour la formation d'intégrateur web sur OpenClassrooms, une
+formation intensive de 9 mois pour devenir développeur Front-end. Cette
+formation m'a permis d'acquérir des compétences en HTML, CSS et
+JavaScript. J'ai également eu l'opportunité de découvrir et de
+travailler avec SASS, React, React-Redux et ses outils. Sans oublier la
+sensibilisation au référencement des sites web et l'amélioration de leur
+visibilité via un projet dédié au SEO.
+<br /> <br />
+Curieux, motivé et avide d'apprendre e nouvelles choses, je poursuis mon
+apprentissage quotidien à la recherche de nouveaux projets, me
+permettant d'enrichir mes connaissances. " */
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 function Footer() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="footer" id="Contact">
       <h4>
@@ -24,7 +35,76 @@ function Footer() {
           </a>
         </nav>
         <span className="App-author">Made with ❤️ by Erwan Robin </span>
+        <span className="legal-mention" onClick={handleModalOpen}>
+          Legal mentions
+        </span>
       </div>
+      {/* Modal component */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleModalClose}
+        contentLabel="DisplayWorkModal"
+        closeTimeoutMS={250}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            backdropFilter: "blur(2px)",
+          },
+          content: {
+            position: "absolute",
+            width: "40%",
+            height: "40%",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            right: "0",
+            bottom: "0",
+            border: "1px solid #ccc",
+            background: "#e0d0c1",
+            overflow: "auto",
+            WebkitOverflowScrolling: "touch",
+            borderRadius: "20px",
+            outline: "none",
+            padding: "20px",
+          },
+        }}
+        className="custom-modal"
+      >
+        <div className="modal__head">
+          <h3>Legal mentions </h3>
+          <button className="modal__head-btnClose" onClick={handleModalClose}>
+            Close
+          </button>
+        </div>
+        <div>
+          <h3>Website Editor</h3>
+          <p>
+            Erwan ROBIN <br />
+            80000 Somewhere <br />
+            Phone number : 0606060606 <br />
+            Email : contact@portfolio-malabeuf.fr <br />
+            Site Web : https://my-portfolio-two-teal.vercel.app/
+          </p>
+        </div>
+        <h3>Host</h3>
+        <p>WebSite hosted with Vercel</p>
+        <p>
+          Formerly ZEIT (2015–2020) <br />
+          Type : Private <br />
+          Industry : Edge computingWeb hostingContent delivery network <br />
+          Headquarters : San Francisco, California, U.S. <br />
+          Area served : Worldwide <br />
+          Key people : Guillermo Rauch (CEO) <br />
+          Website : vercel.com
+        </p>
+        <p>https://vercel.com/</p>
+      </Modal>
     </div>
   );
 }
